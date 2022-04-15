e0ec53e46ee87ef1d224c78f12226bcecb295b97
davlatzoda <davlatzodap@gmail.com>
2022-04-15 17:54:55 +0500

    initial commit

---
+++ checklists/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  checklists
+//
+//  Created by Парвин Давлатзода on 15/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
