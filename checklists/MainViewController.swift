92dd1d152e48b8a0ab3b5ddf5a1b65f5d68a5ffe
davlatzoda <davlatzodap@gmail.com>
2022-04-18 18:11:51 +0500

    Создали ячейки и добавили данные в таблицу

--- checklists/MainViewController.swift
+++ checklists/MainViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class MainViewController: UITableViewController {
 
+    let groups: [CheckListGroup] = [
+        CheckListGroup(title: "Birthdays", imageName: "birthdaypic"),
+        CheckListGroup(title: "Croceries", imageName: "groceeriespic"),
+        CheckListGroup(title: "To Do", imageName: "todopic"),
+        CheckListGroup(title: "Business Stuff", imageName: "pic")
+        
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return groups.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let group: CheckListGroup = groups[indexPath.row]
+        let cell = UITableViewCell()
+        cell.textLabel?.text = group.title
+        return cell
+    }
 }
 
---
+++ checklists/Models/ChecklistGroup.swift
@@ -0,0 +1,13 @@
+//
+//  ChecklistGroup.swift
+//  checklists
+//
+//  Created by Парвин Давлатзода on 18/04/22.
+//
+
+import Foundation
+
+struct CheckListGroup{
+    var title: String
+    var imageName: String
+}
